2aa04be6f02995d29f979dfdda0299f3694c73af
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-06-10 22:53:39 +1200

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -4,8 +4,8 @@ export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers = {
-    "X-RapidAPI-Key": "3bdf99826fmsh241e5283b3cd6b3p1a95c7jsn4331926ed84e",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY || "",
+    "X-RapidAPI-Host": process.env.RAPID_API_HOST || "",
   };
 
   const response = await fetch(
